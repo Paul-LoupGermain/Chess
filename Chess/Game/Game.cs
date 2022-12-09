9099ec81e3c2171e353b2426149be1d33e9b518b
Le coq <Amos.LE-COQ@cpnv.ch>
2022-12-09 11:45:20 +0100

    Add menu

--- Chess/Game/Game.cs
+++ Chess/Game/Game.cs
@@ -2,96 +2,100 @@
 
 namespace GameTest
 {
-    public class Game
+    public class PlayGame
     {
-        static private Table tableCase = new Table();
+        private Table tableCase = new Table();
 
-        public void game()
+        public void Game()
         {
             bool fin = false;
-            while (fin == false)
-            {
-
-                afficher();
+            do{
+                Afficher();
                 string position = Console.ReadLine();
-
-                string positionBase = position.Split("-")[0];
-                string positionFin = position.Split("-")[1];
-
-                //  MessageBox.Show("position de base: " + positionBase + " position de fin: " + positionFin);
-
-                string positionFinString = positionFin.Substring(1);
-                string positionBaseString = positionBase.Substring(1);
-
-                //Définir position de fin string et position de base string en int
-                int numFin = int.Parse(positionFinString);
-                int numBase = int.Parse(positionBaseString);
-
-                string a = coordonnees(positionBase);
-                //Console.WriteLine(a+" Base");
-
-                // Position de Base ex. A2 = x:0 et y:1
-                int xBase = int.Parse(a.Split("-")[0]);
-                int yBase = int.Parse(a.Split("-")[1]);
-
-                string b = coordonnees(positionFin);
-                //Console.WriteLine(b+" Fin");
-
-                // Position de Fin ex. A2 = x:0 et y:1
-                int xFin = int.Parse(b.Split("-")[0]);
-                int yFin = int.Parse(b.Split("-")[1]);
-
-                //Trouver la pièce qui correspond au cooronnées
-                // string pieceBase = tabPawn[yBase, xBase];
-                string pieceBase = tableCase.TabPawn[yBase, xBase];
-                string pieceFin = tableCase.TabPawn[yFin, xFin];
-                //Console.WriteLine(pieceBase+"-"+pieceFin);
-
-                switch (pieceBase)
+                
+                if ((position != "Q")&&(position!="q"))
                 {
-                    case "P":
-                        if (numBase > numFin)
-                        {
-                            Console.WriteLine("\nErreur: le pion ne peu pas reculer");
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            //Si une pièce est devant alors il peut pas avencer
-                            if (pieceFin == " ")
+                    string positionBase = position.Split("-")[0];
+                    string positionFin = position.Split("-")[1];
+
+                    //MessageBox.Show("position de base: " + positionBase + " position de fin: " + positionFin);
+
+                    string positionFinString = positionFin.Substring(1);
+                    string positionBaseString = positionBase.Substring(1);
+
+                    //  Définir position de fin string et position de base string en int
+                    int numFin = int.Parse(positionFinString);
+                    int numBase = int.Parse(positionBaseString);
+
+                    string a = Coordonnees(positionBase);
+                    //Console.WriteLine(a+" Base");
+
+                    //  Position de Base ex. A2 = x:0 et y:1
+                    int xBase = int.Parse(a.Split("-")[0]);
+                    int yBase = int.Parse(a.Split("-")[1]);
+
+                    string b = Coordonnees(positionFin);
+                    //Console.WriteLine(b+" Fin");
+
+                    //  Position de Fin ex. A2 = x:0 et y:1
+                    int xFin = int.Parse(b.Split("-")[0]);
+                    int yFin = int.Parse(b.Split("-")[1]);
+
+                    //  Trouver la pièce qui correspond au cooronnées
+                    string pieceBase = tableCase.TabPawn[yBase, xBase];
+                    string pieceFin = tableCase.TabPawn[yFin, xFin];
+                    //Console.WriteLine(pieceBase+"-"+pieceFin);
+
+                    switch (pieceBase)
+                    {
+                        case "P":
+                            if (numBase > numFin)
                             {
-                                //Console.WriteLine(yBase + "-" + yFin);
-                                if (yBase == 1 && yFin == 3)
+                                Console.WriteLine("\nErreur: le pion ne peu pas reculer");
+                                Console.ReadKey();
+                            }
+                            else
+                            {
+                                //  Si une pièce est devant alors il peut pas avencer
+                                if (pieceFin == " ")
                                 {
-                                    tableCase.TabPawn[yFin, xFin] = "P";
-                                    tableCase.TabPawn[yBase, xBase] = " ";
-                                }
-                                else
-                                {
-                                    if (yBase + 1 == yFin)
+                                    //Console.WriteLine(yBase + "-" + yFin);
+                                    if (yBase == 1 && yFin == 3)
                                     {
                                         tableCase.TabPawn[yFin, xFin] = "P";
                                         tableCase.TabPawn[yBase, xBase] = " ";
                                     }
                                     else
                                     {
-                                        Console.WriteLine("\nErreur: le pion ne peu pas avancer de plus de 2 cases");
-                                        Console.ReadKey();
+                                        if (yBase + 1 == yFin)
+                                        {
+                                            tableCase.TabPawn[yFin, xFin] = "P";
+                                            tableCase.TabPawn[yBase, xBase] = " ";
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("\nErreur: le pion ne peu pas avancer de plus de 2 cases");
+                                            Console.ReadKey();
+                                        }
                                     }
                                 }
+                                else
+                                {
+                                    Console.WriteLine("\nErreur: le pion ne peu pas avancer car il y a une pièce devant");
+                                    Console.ReadKey();
+                                }
                             }
-                            else
-                            {
-                                Console.WriteLine("\nErreur: le pion ne peu pas avancer car il y a une pièce devant");
-                                Console.ReadKey();
-                            }
-                        }
                         break;
+                    }
                 }
-            }
+                else
+                {
+                    fin = true;
+                }
+            } while (fin == false);
         }
 
-        private string coordonnees(string pos)
+        private string Coordonnees(string pos)
         {
             for (int x = 0; x < 8; x++)
             {
@@ -107,7 +111,7 @@ namespace GameTest
             return "404";
         }
 
-        private void afficher()
+        private void Afficher()
         {
             Console.Clear();
             Console.Write("   ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n");
--- Chess/TestChess/Program.cs
+++ Chess/TestChess/Program.cs
@@ -7,7 +7,38 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Game start = new Game();
-        start.game();
+        while (true) {
+            Console.Clear();
+            AfficherMenu();
+            string choix = Console.ReadLine();
+            switch (choix)
+            {
+                case "1":
+                    PlayGame game = new PlayGame();
+                    game.Game();
+                    break;
+                case "2":
+                    Console.WriteLine("\n En construction\n");
+                    Console.WriteLine(" Appuyer sur n'importe quelle touche pour continuer...\n");
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    return;
+            }
+        }
+    }
+
+    static private void AfficherMenu()
+    {
+        Console.WriteLine("\n ╔══════════════╗");
+        Console.WriteLine(" ║ Jouer    : 1 ║");
+        Console.WriteLine(" ╚══════════════╝\n");
+        Console.WriteLine(" ╔══════════════╗");
+        Console.WriteLine(" ║ Info     : 2 ║");
+        Console.WriteLine(" ╚══════════════╝\n");
+        Console.WriteLine(" ╔══════════════╗");
+        Console.WriteLine(" ║ Quitter  : 3 ║");
+        Console.WriteLine(" ╚══════════════╝\n");
+        Console.Write(" ► ");
     }
 }
